8bfcfcd22171eef9e6b88f8367268b224b27e786
Jared Hanson and Jimmy Zebroski <jimmy@>
2019-01-24 08:50:48 -0800

    allow functionality to leap years

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-  $("#run").submit(function() {
 
-    var input = parseInt($("#input").val());
-    return false;
+
+  $("#run").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("#input").val());
+    var leapYear = function(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return " is a leap year.";
+      }
+      else {
+        return " is not a leap year.";
+      }
+    };
+
+    var result = leapYear(year);
+
+
+    $("#result").text(year + result);
 
 
 
